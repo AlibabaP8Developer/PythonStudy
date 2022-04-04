ea815a2626866f543db5521f969385d40f3f88d1
lizhenghang <1135365441@qq.com>
2022-04-04 14:26:00 +0800

    格式化符号

--- basic/02格式化符号.py
+++ basic/02格式化符号.py
@@ -19,12 +19,21 @@ weight = 60
 stuId = 111222333
 
 # 今年朱元璋的年龄是10岁
-print("今年%s的年龄是%d岁", name, age)
+print("今年%s的年龄是%d岁" % (name, age))
 
 # 朱元璋的太子名字是朱标
 print("朱元璋的太子名字是%s" % son)
+print("%s的太子名字是%s" % (name, son))
 
 # 朱元璋的体重是60kg
 print("朱元璋的体重是%dkg" % weight)
+print("明年朱元璋的体重是%dkg" % (weight + 10))
 
 # 朱元璋的学号是111222333
+print("朱元璋的学号是%d" % stuId)
+
+print("%s的太子是%s，%s今年%d岁" % (name, son, son, age))
+
+# f"{表达式}"
+print("f{表达式}的方式 ==> " + f"{name}的太子是{son}，{son}今年{age}岁，明年{son}是{age+1}岁了")
+
