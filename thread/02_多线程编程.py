5caf6a77f6ef5c22c05d4bd041f8685326b55f1e
Edwin <1135365441@qq.com>
2024-01-07 16:16:31 +0800

    多线程

--- thread/02_多线程编程.py
+++ thread/02_多线程编程.py
@@ -4,19 +4,22 @@
 import time
 import threading
 
-def sing():
+
+def sing(msg):
     while True:
-        print('sing...')
+        print(msg + 'sing...')
         time.sleep(1)
 
-def dance():
+
+def dance(msg):
     while True:
-        print('dance...')
+        print(msg + '...')
         time.sleep(1)
+
 
 if __name__ == '__main__':
     # 创建sing线程
-    sing_thread = threading.Thread(target=sing)
-    dance_tread = threading.Thread(target=dance)
+    sing_thread = threading.Thread(target=sing, args=('唱歌',))
+    dance_tread = threading.Thread(target=dance, kwargs={'msg':'道家无上心法'})
     sing_thread.start()
     dance_tread.start()
